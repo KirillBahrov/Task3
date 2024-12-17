ac62f61f339645289b73f3c9d1b8b870a1ea503c
KirillBahrov <kirillbahrow@gmail.com>
2024-12-18 00:03:41 +0300

    Создал 3 задание вроде работает

---
+++ Solver/Exercise.h
@@ -0,0 +1,65 @@
+﻿#pragma once
+#include "Matrix.h"
+#include "Generator.h"
+
+namespace RUT
+{
+    template <typename T>
+    /**
+     * @brief Класс Exercise представляет абстрактный базовый класс для работы с матрицами.
+     * @tparam T Тип данных, используемый в матрице.
+     */
+    class Exercise {
+    public:
+        Matrix<T> matrix;
+        Generator<T>& generator;
+
+    
+        /**
+         * @brief Конструктор класса Exercise.
+         * @param rows Количество строк в матрице.
+         * @param cols Количество столбцов в матрице.
+         * @param gen Ссылка на объект генератора значений.
+         */
+        Exercise(size_t rows, size_t cols, Generator<T>& gen) : matrix(rows, cols), generator(gen) {}
+
+        /**
+        * * @brief Деструктор класса Exercise (виртуальный).
+         */
+        virtual ~Exercise() = default;
+
+        /**
+         * @brief Чисто виртуальная функция для выполнения задачи 1.
+         */
+        virtual void Task1() = 0;
+
+        /**
+         * @brief Чисто виртуальная функция для выполнения задачи 2.
+         */
+        virtual void Task2() = 0;
+
+        /**
+         * @brief Заполняет матрицу случайными значениями, используя генератор.
+         */
+        void fillMatrix() {
+            for (size_t i = 0; i < matrix.getRows(); ++i)
+                for (size_t j = 0; j < matrix.getCols(); ++j)
+                    matrix[i][j] = generator.generate();
+        }
+
+        /*/**
+        * @brief функция получения указателя на матрицу
+        
+        getMatrix() const
+        {
+            return matrix;
+        }*/
+
+        /**
+         * @brief Выводит содержимое матрицы на стандартный вывод.
+         */
+        void printMatrix() const {
+            matrix.print();
+        }
+    };
+}
---
+++ TASK3/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <locale>
+#include "../Solver/RandomGenerator.h"
+#include "../Solver/IStreamGeneretor.h"
+#include "../Solver/MyExercise.h"
+
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    RUT::RandomGenerator<int> intGen(-100, 100);
+    RUT::MyExercise<int> intExercise(3, 3, intGen);
+
+    std::cout << "Матрица с случайными значениями (int):\n";
+    intExercise.fillMatrix();
+    intExercise.printMatrix();
+
+    std::cout << "\nПосле Task1 (замена максимального по модулю элемента на противоположный):\n";
+    intExercise.Task1();
+    intExercise.printMatrix();
+
+    std::cout << "\nПосле Task2 (вставка первой строки после каждой чётной строки):\n";
+    intExercise.Task2();
+    intExercise.printMatrix();
+
+    std::cout << "\nВведите значения для матрицы 2x2 (double):\n";
+    RUT::IStreamGenerator<double> userInputGen;
+    RUT::MyExercise<double> doubleExercise(2, 2, userInputGen);
+
+    doubleExercise.fillMatrix();
+    std::cout << "Матрица, введённая пользователем:\n";
+    doubleExercise.printMatrix();
+
+    return 0;
+}
